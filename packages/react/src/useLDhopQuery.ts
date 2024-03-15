b78ebe1b399f7df6a8f497541e3e5c4c9f214bb3
mrkvon <mrkvon@protonmail.com>
2024-03-16 00:55:12 +0100

    Improve useLDhopQuery based on trying it out

--- packages/react/src/useLDhopQuery.ts
+++ packages/react/src/useLDhopQuery.ts
@@ -1,26 +1,31 @@
 import { QueryAndStore, RdfQuery } from '@ldhop/core'
 import { fetchRdfDocument } from '@ldhop/core/dist/utils/helpers'
-import { QueryKey, useQueries } from '@tanstack/react-query'
+import { QueryKey, UseQueryResult, useQueries } from '@tanstack/react-query'
 import mapValues from 'lodash/mapValues'
-import type { Store } from 'n3'
+import { Quad, Store } from 'n3'
 import { useEffect, useMemo, useRef, useState } from 'react'
 
 type Fetch = typeof globalThis.fetch
 
 type Variables = { [variable: string]: string[] | undefined }
+const defaultGetAdditionalData = () => ({})
 
-export const useLDhopQuery = ({
+export const useLDhopQuery = <AdditionalData extends object = object>({
   query,
   variables,
   fetch,
-  getQueryKey = resource => ['rdfResource', resource],
+  getQueryKey = resource => ['rdfDocument', resource],
   staleTime = Infinity,
+  getAdditionalData = defaultGetAdditionalData as () => AdditionalData,
 }: {
   query: RdfQuery
   variables: Variables
   fetch: Fetch
   getQueryKey?: (resource: string) => QueryKey
   staleTime?: number
+  getAdditionalData?: (
+    results: UseQueryResult<{ data: Quad[]; response: Response }, Error>[],
+  ) => AdditionalData
 }) => {
   const variableSets = useMemo(
     () => mapValues(variables, array => new Set(array)),
@@ -29,24 +34,20 @@ export const useLDhopQuery = ({
 
   const [resources, setResources] = useState<string[]>([])
   const [outputVariables, setOutputVariables] = useState<Variables>({})
-  const [outputStore, setOutputStore] = useState<Store>()
+  const [outputStore, setOutputStore] = useState<Store>(new Store())
 
   const results = useQueries({
     queries: resources.map(resource => ({
       queryKey: getQueryKey(resource),
-      queryFn: async () => ({
-        ...(await fetchRdfDocument(resource, fetch)),
-        resource,
-      }),
+      queryFn: () => fetchRdfDocument(resource, fetch),
       staleTime,
     })),
     combine: results => ({
+      ...(getAdditionalData?.(results) ?? {}),
       data: results
-        .filter(result => result.status === 'success' && result.data)
-        .map(result => ({
-          data: result.data!.data,
-          resource: result.data!.resource,
-        })),
+        .map((result, i) => [result, resources[i]] as const)
+        .filter(([result]) => result.status === 'success' && result.data)
+        .map(([result, resource]) => ({ data: result.data!.data, resource })),
       pending: results.some(result => result.isPending),
     }),
   })
@@ -94,7 +95,8 @@ export const useLDhopQuery = ({
       store: outputStore,
       variables: outputVariables,
       qas: qas.current,
+      isLoading: results.pending,
     }),
-    [outputStore, outputVariables],
+    [outputStore, outputVariables, results.pending],
   )
 }
